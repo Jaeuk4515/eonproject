1239cc870b24691736352ecc6baff27e0f3575c9
Jaeuk4515 <65009983+Jaeuk4515@users.noreply.github.com>
2020-06-26 14:39:59 +0900

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,23 +1,31 @@
-n = int(input("작업수를 입력하세요 : "))
-m = int(input("작업번호를 입력하세요 : "))
+list_number = int(input('수를 입력하세요 : '))
+data = [[0] * list_number for i in range(list_number)]
+column = 0
+row = -1
+value = 0
+way = 1
 
-if (0 < n <= 100 and 0 <= m <= n-1):
-    rank = list(map(int, input("우선순위를 입력하세요 : ").split()))
-    minute = 0
-    number = list(range(len(rank)))
-    
-    while True:
-        if rank[0] < max(rank):
-            rank.append(rank.pop(0))
-            number.append(number.pop(0))
-        else:
-            minute = minute + 1
-            if number[0] == m:
-                print(minute, "분")
-                break
-            else:
-                rank.pop(0)
-                number.pop(0)
+def snail_array(column, row, value, way, list_number):
+    for i in range(0, list_number):
+        value += 1
+        row = row + way
+        data[column][row] = value
 
-else:
-    print("값을 잘못 입력하셨습니다.")
+    list_number -= 1
+
+    for i in range(0, list_number):
+        value += 1
+        column = column + way
+        data[column][row] = value
+
+    way = way * -1
+
+    if list_number == 0:
+        return 0
+
+    snail_array(column, row, value, way, list_number)
+
+snail_array(column, row, value, way, list_number)
+
+for column in range(list_number):
+    print(data[column])
